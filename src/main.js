5897dbfd04752a4782b7f970133d2f4bbbd357f1
Xu Jing <xjyammia@gmail.com>
2026-08-18 23:30:15 +0700

    2

--- src/main.js
+++ src/main.js
@@ -47,14 +47,14 @@ autoUpdater.on('update-available', (info) => {
 })
 autoUpdater.on('update-not-available', () => {})
 autoUpdater.on('error', (err) => {
-  const errMsg = err ? err.message : '未知错误'
+  const errMsg = err ? (err.stack || err.message || JSON.stringify(err)) : '未知错误'
   try {
     mainWindow.webContents.send('update-error')
     dialog.showMessageBoxSync(mainWindow, {
       type: 'warning',
       title: '更新失败',
       message: '自动更新下载失败',
-      detail: `错误信息：${errMsg}\n\n请稍后重启软件重试，或前往 GitHub 手动下载新版本。\nhttps://github.com/xjyammia-star/Obsidian-/releases/latest`,
+      detail: `${errMsg}\n\n请前往 GitHub 手动下载：\nhttps://github.com/xjyammia-star/Obsidian-/releases/latest`,
       buttons: ['确定']
     })
   } catch(_) {}
